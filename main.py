4787fd1aa45427df10fdefd0c9804063ebabbd99
Angel Trendafilov <aatrendafilov@gmail.com>
2024-03-20 14:36:44 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,28 +1,24 @@
 from flask import Flask, render_template
-from post import Post
 import requests
 
-posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
-post_objects = []
-for post in posts:
-    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
-    post_objects.append(post_obj)
+posts = requests.get("https://api.npoint.io/0723d0fa440771f1ac25").json()
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def get_all_posts():
-    return render_template("index.html", all_posts=post_objects)
+    return render_template("index.html", all_posts=posts)
 
 
-@app.route("/post/<int:index>")
-def show_post(index):
-    requested_post = None
-    for blog_post in post_objects:
-        if blog_post.id == index:
-            requested_post = blog_post
-    return render_template("post.html", post=requested_post)
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
