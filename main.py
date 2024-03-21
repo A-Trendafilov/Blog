ccd720b5594a29b56c8f5d59cb729e1972f7d272
Angel Trendafilov <aatrendafilov@gmail.com>
2024-03-21 12:38:14 +0200

    Initial contact form.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
-import requests
+import os
+import smtplib
 
+import requests
+from dotenv import load_dotenv
+from flask import Flask, render_template, request
+
+load_dotenv()
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
 posts = requests.get("https://api.npoint.io/0723d0fa440771f1ac25").json()
 
 app = Flask(__name__)
@@ -16,9 +23,24 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_send=True)
+    return render_template("contact.html", msg_send=False)
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(EMAIL, PASSWORD)
+            connection.sendmail(
+                from_addr=EMAIL,
+                to_addrs="aatrendafilov.pytest@gmail.com",
+                msg=email_message.encode("utf-8"),
+            )
 
 
 @app.route("/post/<int:index>")
@@ -29,5 +51,7 @@ def show_post(index):
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
 
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
