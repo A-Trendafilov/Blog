e065f1881d9f20250b513adf837bcd9816aa7bcc
Angel Trendafilov <aatrendafilov@gmail.com>
2024-03-30 16:52:00 +0200

    Adding new futures: Add/Edit/Delete -> post. Adding Database for post.

--- README.md
+++ README.md
@@ -1,3 +1,127 @@
-# Flask Blog
+# Blog Project
 
- This is a simple Flask blog application that retrieves posts from an external API and displays them on the web interface.
+## Prerequisites
+
+Before running the project, ensure you have the following installed:
+
+- Python 3.x
+- Flask
+- Flask-Bootstrap5
+- Flask-SQLAlchemy
+- Flask-WTF
+- Flask-CKEditor
+- SQLAlchemy
+- dotenv
+
+You can install the dependencies using pip:
+
+```bash
+pip install Flask Flask-Bootstrap5 Flask-SQLAlchemy Flask-WTF Flask-CKEditor SQLAlchemy python-dotenv
+```
+
+### Setup
+
+- Clone this repository to your local machine:
+
+```bash
+git clone https://github.com/your_username/blog_project.git
+```
+
+- Navigate to the project directory:
+
+```bash
+cd blog_project
+```
+
+- Create a virtual environment:
+
+```bash
+python -m venv venv
+```
+
+- Activate the virtual environment:
+    - On Windows:
+      ```bash
+      venv\Scripts\activate
+      ``` 
+    - On macOS and Linux:
+      ```bash
+      source venv/bin/activate
+      ```
+
+- Create a .env file in the project directory and set the following variables:
+
+```makefile
+SECRET_KEY=your_secret_key
+EMAIL=your_email
+PASSWORD=your_email_password
+```
+
+Replace your_secret_key, your_email, and your_email_password with your preferred values. Make sure to use a secure and
+strong secret key.
+
+- Initialize the SQLite database by running:
+
+```bash
+python
+```
+
+Then, inside the Python shell:
+
+```python
+from app import db
+
+db.create_all()
+exit()
+```
+
+### Running the Application
+
+To start the Flask server, run:
+
+```bash
+python app.py
+```
+
+The application will be accessible at http://localhost:5003 in your web browser.
+
+## Usage
+
+### Creating a New Post
+
+- Navigate to the homepage.
+- Click on "New Post".
+- Fill in the required fields: title, subtitle, author, blog image URL, and content.
+- Click on "Submit Post" to create the post.
+
+### Editing a Post
+
+- Navigate to the post you want to edit.
+- Click on the "Edit" button.
+- Modify the content as needed.
+- Click on "Submit Post" to save the changes.
+
+### Deleting a Post
+
+- Navigate to the post you want to delete.
+- Click on the "Delete" button.
+- Contact Form
+- Navigate to the "Contact" page.
+- Fill in your name, email, phone (optional), and message.
+- Click on "Send Message" to submit the contact form.
+
+### About
+
+- The "About" page provides brief information about the blog project.
+
+### Additional Notes
+
+- This project utilizes Flask for its backend framework.
+- Bootstrap5 is used for styling.
+- Flask-SQLAlchemy manages the database interactions.
+- Flask-WTF is used for form handling.
+- Flask-CKEditor is integrated for a rich text editor.
+- Contact form functionality is implemented using Flask-Mail.
+- Ensure you replace the email and password in the .env file with valid credentials for the contact form to work
+  properly.
+
--- main.py
+++ main.py
@@ -1,21 +1,120 @@
-import os
-import smtplib
-
-import requests
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Text
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+from datetime import date
 from dotenv import load_dotenv
-from flask import Flask, render_template, request
+import smtplib
+import os
 
 load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
 EMAIL = os.getenv("EMAIL")
 PASSWORD = os.getenv("PASSWORD")
-posts = requests.get("https://api.npoint.io/0723d0fa440771f1ac25").json()
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = SECRET_KEY
+ckeditor = CKEditor(app)
+Bootstrap5(app)
+
+
+# CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///posts.db"
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+# CONFIGURE TABLE
+class BlogPost(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
+    date: Mapped[str] = mapped_column(String(250), nullable=False)
+    body: Mapped[str] = mapped_column(Text, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+# CREATE FORMS
+class PostForm(FlaskForm):
+    title = StringField(label="Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField(label="Subtitle", validators=[DataRequired()])
+    author = StringField("Your name", validators=[DataRequired()])
+    img_url = StringField("Blog image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField(label="Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
 
 
 @app.route("/")
 def get_all_posts():
+    posts = db.session.execute(db.select(BlogPost)).scalars().all()
     return render_template("index.html", all_posts=posts)
+
+
+@app.route("/post/<int:post_id>")
+def show_post(post_id):
+    requested_post = db.get_or_404(BlogPost, post_id)
+    return render_template("post.html", post=requested_post)
+
+
+@app.route("/new-post", methods=["POST"])
+def add_new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        new_post = BlogPost(
+            title=form.title.data,
+            subtitle=form.subtitle.data,
+            date=date.today().strftime("%B %d, %Y"),
+            body=form.body.data,
+            author=form.author.data,
+            img_url=form.img_url.data,
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for("get_all_posts"))
+    return render_template("make-post.html", form=form)
+
+
+@app.route("/edit-post/<int:post_id>/", methods=["GET", "POST"])
+def edit_post(post_id):
+    post = db.get_or_404(BlogPost, post_id)
+    edit_form = PostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        author=post.author,
+        img_url=post.img_url,
+        body=post.body,
+    )
+    if edit_form.validate_on_submit():
+        post.title = edit_form.title.data
+        post.subtitle = edit_form.subtitle.data
+        post.author = edit_form.author.data
+        post.img_url = edit_form.img_url.data
+        post.body = edit_form.body.data
+        db.session.commit()
+        return redirect(url_for("show_post", post_id=post.id))
+    return render_template("make-post.html", form=edit_form, is_edit=True)
+
+
+@app.route("/delete/<int:post_id>")
+def delete_post(post_id):
+    post_to_delete = db.get_or_404(BlogPost, post_id)
+    db.session.delete(post_to_delete)
+    db.session.commit()
+    return redirect(url_for("get_all_posts"))
 
 
 @app.route("/about")
@@ -23,35 +122,26 @@ def about():
     return render_template("about.html")
 
 
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(EMAIL, PASSWORD)
+        connection.sendmail(
+            from_addr=EMAIL,
+            to_addrs=EMAIL,
+            msg=email_message.encode("utf-8"),
+        )
+
+
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
         data = request.form
         send_email(data["name"], data["email"], data["phone"], data["message"])
-        return render_template("contact.html", msg_send=True)
-    return render_template("contact.html", msg_send=False)
-
-def send_email(name, email, phone, message):
-    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-            connection.starttls()
-            connection.login(EMAIL, PASSWORD)
-            connection.sendmail(
-                from_addr=EMAIL,
-                to_addrs="aatrendafilov.pytest@gmail.com",
-                msg=email_message.encode("utf-8"),
-            )
-
-
-@app.route("/post/<int:index>")
-def show_post(index):
-    requested_post = None
-    for blog_post in posts:
-        if blog_post["id"] == index:
-            requested_post = blog_post
-    return render_template("post.html", post=requested_post)
-
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5003)
