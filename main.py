a0013aeebc1f0a2d3c87132dfaf1b81f67940af1
Angel Trendafilov <aatrendafilov@gmail.com>
2024-04-04 10:39:51 +0300

    Add Procfile for gunicorn

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import os
+import smtplib
 from datetime import date
+from functools import wraps
+
+from dotenv import load_dotenv
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
@@ -12,19 +17,17 @@ from flask_login import (
     login_required,
 )
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Text
-from functools import wraps
+from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 from werkzeug.security import generate_password_hash, check_password_hash
+
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
-from dotenv import load_dotenv
-import os
-import smtplib
 
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 EMAIL = os.getenv("EMAIL")
 PASSWORD = os.getenv("PASSWORD")
+DB_URI = os.getenv("DB_URI")
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = SECRET_KEY
@@ -57,7 +60,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = DB_URI
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -273,4 +276,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5003)
+    app.run(debug=False)
